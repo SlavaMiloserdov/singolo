921a5443ae217b5d77fd6bbff848b247af00e707
SlavaMiloserdov <slavmiloserdov@mail.ru>
2020-03-15 18:00:05 +0400

    feat: add carousel

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Header
 const navigationHandler = event => {
   const menu = document.querySelectorAll(".navigation__link > a");
   menu.forEach(item => {
@@ -11,3 +12,60 @@ const navigationHandler = event => {
 document
   .querySelector(".navigation")
   .addEventListener("click", navigationHandler);
+// Header
+
+// Carousel
+let slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+let isEnabled = true;
+
+function changeCurrentSlide(n) {
+  currentSlide = (n + slides.length) % slides.length; // For infinity carousel
+}
+
+function hideSlide(direction) {
+  isEnabled = false;
+  slides[currentSlide].classList.add(direction);
+  slides[currentSlide].addEventListener("animationend", function() {
+    this.classList.remove("active", direction);
+  });
+}
+
+function showSlide(direction) {
+  slides[currentSlide].classList.add("next", direction);
+  slides[currentSlide].addEventListener("animationend", function() {
+    this.classList.remove("next", direction);
+    this.classList.add("active");
+    isEnabled = true;
+  });
+}
+
+function nextSlide(n) {
+  hideSlide("to-left");
+  changeCurrentSlide(n + 1);
+  showSlide("from-right");
+}
+
+function previousSlide(n) {
+  hideSlide("to-right");
+  changeCurrentSlide(n - 1);
+  showSlide("from-left");
+}
+
+document
+  .querySelector(".slider__button_prev")
+  .addEventListener("click", function() {
+    if (isEnabled) {
+      previousSlide(currentSlide);
+    }
+  });
+
+document
+  .querySelector(".slider__button_next")
+  .addEventListener("click", function() {
+    if (isEnabled) {
+      nextSlide(currentSlide);
+    }
+  });
+
+// Carousel
