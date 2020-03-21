31473ac5094b5b64b4dc64828ee22a11e943248d
SlavaMiloserdov <slavmiloserdov@mail.ru>
2020-03-21 20:45:53 +0400

    refactor: change form

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // Header
-document.addEventListener("scroll", onScroll);
-
-function onScroll(event) {
+const onScroll = () => {
   let currentPosition = window.scrollY;
   const menu = document.querySelectorAll(".navigation__link > a");
   const elements = document.querySelectorAll(".wrapper>section");
@@ -25,7 +23,9 @@ function onScroll(event) {
       menu[menu.length - 1].classList.add("navigation__link_checked");
     }
   });
-}
+};
+
+document.addEventListener("scroll", onScroll);
 // Header
 
 // Slider
@@ -104,61 +104,71 @@ document
       event.target.closest(".slider").classList.add("slider_background-blue");
     }
   });
-
 // Carousel
 
-let iphonesVertical = document.querySelectorAll(".slide__iphone-vertical");
+const iphonesVertical = document.querySelectorAll(".slide__iphone-vertical");
+const iphonesHorizontal = document.querySelectorAll(
+  ".slide__iphone-horizontal"
+);
+
+const onClickVerticalIphone = () => {
+  iphonesVertical.forEach(item => {
+    item.classList.toggle("slide__iphone-vertical_off");
+  });
+};
+
+const onClickHorizontalIphone = () => {
+  iphonesHorizontal.forEach(item => {
+    item.classList.toggle("slide__iphone-horizontal_off");
+  });
+};
+
 document
   .querySelector(".button_vertical-iphone")
-  .addEventListener("click", () => {
-    iphonesVertical.forEach(item => {
-      item.classList.toggle("slide__iphone-vertical_off");
-    });
-  });
+  .addEventListener("click", onClickVerticalIphone);
 
-let iphonesHorizontal = document.querySelectorAll(".slide__iphone-horizontal");
 document
   .querySelector(".button_horizontal-iphone")
-  .addEventListener("click", () => {
-    iphonesHorizontal.forEach(item => {
-      item.classList.toggle("slide__iphone-horizontal_off");
-    });
-  });
+  .addEventListener("click", onClickHorizontalIphone);
 // Slider
 
 // Portfolio
 const picturesPortfolio = document.querySelectorAll(".portfolio__image");
-var counter = 0;
 
 const portfolioNavigationHandler = event => {
   event.preventDefault();
-  const menu = document.querySelectorAll(".portfolio-navigation__category");
-  menu.forEach(item => {
+  const tags = document.querySelectorAll(".portfolio-navigation__category");
+
+  tags.forEach(item => {
     if (
       (event.target === item &&
         !item.classList.contains("portfolio-navigation__category_checked")) ||
       (event.target === item.firstElementChild &&
         !item.classList.contains("portfolio-navigation__category_checked"))
     ) {
-      menu.forEach(item =>
+      tags.forEach(item =>
         item.classList.remove("portfolio-navigation__category_checked")
       );
       item.classList.add("portfolio-navigation__category_checked");
 
-      for (let i = 0; i < picturesPortfolio.length; i++) {
-        if (counter === 11) {
-          picturesPortfolio.forEach(pict => pict.classList.remove("order"));
-          counter = 0;
-        }
-        if (!picturesPortfolio[i].classList.contains("order")) {
-          picturesPortfolio[i].classList.add("order");
-          counter++;
-          break;
-        }
-      }
+      updatePictures();
     }
   });
 };
+
+const updatePictures = () => {
+  let newPictures = picturesPortfolio;
+  picturesPortfolio.forEach(picture => picture.remove());
+
+  newPictures = shuffle(Array.from(newPictures));
+  newPictures.forEach(picture => {
+    document.querySelector(".layout-4-column").append(picture);
+  });
+};
+
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
 
 document
   .querySelector(".portfolio-navigation__categories")
@@ -181,47 +191,7 @@ picturesPortfolio.forEach(picture =>
 // Portfolio
 
 // Get a Quote
-
-const submitHandler = event => {
-  event.preventDefault();
-
-  // let subject = event.target.elements[2].value || "Without subject";
-  // let description = event.target.elements[3].value || "Without description";
-
-  // if (event.target.elements[2].value === "Singolo") {
-  //   subject = "Subject: Singolo";
-  // }
-
-  // if (event.target.elements[3].value === "Portfolio project") {
-  //   description = "Description: Portfolio project";
-  // }
-
-  let subject = event.target.elements[2].value;
-  let description = event.target.elements[3].value;
-  switch (subject) {
-    case "Singolo":
-      subject = "Subject: Singolo";
-      break;
-    case "":
-      subject = "Without subject";
-      break;
-    default:
-      subject = `Subject: ${subject}`;
-      break;
-  }
-
-  switch (description) {
-    case "Portfolio project":
-      description = "Description: Portfolio project";
-      break;
-    case "":
-      description = "Without description";
-      break;
-    default:
-      description = `Description: ${description}`;
-      break;
-  }
-
+function createModalWindow(subject, description) {
   let fragment = document.createDocumentFragment();
   let modalWindow = document.createElement("div");
   let modalContent = document.createElement("div");
@@ -231,21 +201,14 @@ const submitHandler = event => {
   contentSubject.textContent = subject;
   let contentDescription = document.createElement("p");
   contentDescription.textContent = description;
-
   let modalButton = document.createElement("button");
   modalButton.textContent = "OK";
-  modalButton.classList.add("modal-button");
 
-  modalWindow.style =
-    "position: fixed; z-index: 1;width: 100%;height: 100%;overflow: auto;background-color: rgba(0,0,0,0.4);left: 0;top: 0;";
-  modalContent.style =
-    "background-color: #fff; margin: auto; padding: 20px; border: 1px solid #888; width: 60%; border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;";
-  contentSubject.style =
-    "text-overflow: ellipsis; overflow: hidden; text-align: justify;";
-  contentDescription.style =
-    "text-overflow: ellipsis; overflow: hidden; text-align: justify;";
-  modalButton.style =
-    "color: #767e9e; font-size: 28px; font-weight: bold; border: 1px solid #767e9e; border-radius: 7px;";
+  modalWindow.classList.add("modal");
+  modalContent.classList.add("modal__content");
+  contentSubject.classList.add("modal__subject");
+  contentDescription.classList.add("modal__description");
+  modalButton.classList.add("modal__button");
 
   modalButton.onmouseover = function() {
     this.classList.add("cursor-pointer");
@@ -260,13 +223,39 @@ const submitHandler = event => {
   fragment.appendChild(modalWindow);
   document.body.appendChild(fragment);
 
-  document.querySelector(".modal-button").addEventListener("click", () => {
-    document.querySelector(".modal-window").remove();
+  document.querySelector(".modal__button").addEventListener("click", () => {
+    document.querySelector(".modal").remove();
+    document.querySelector(".contact-form").reset();
   });
+}
+
+const submitHandler = event => {
+  event.preventDefault();
+
+  let subject = event.target.elements[2].value;
+  let description = event.target.elements[3].value;
+  switch (subject) {
+    case "":
+      subject = "No subject";
+      break;
+    default:
+      subject = `Subject: ${subject}`;
+      break;
+  }
+
+  switch (description) {
+    case "":
+      description = "No description";
+      break;
+    default:
+      description = `Description: ${description}`;
+      break;
+  }
+
+  createModalWindow(subject, description);
 };
 
 document
   .querySelector(".contact-form")
   .addEventListener("submit", submitHandler);
-
 // Get a Quote
